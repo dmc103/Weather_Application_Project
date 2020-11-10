50bf91b2a489a5dd78177dbb7b135bf16f5a2840
D Janssen <doreenmarie_cee@hotmail.com>
2020-11-10 22:03:03 +0100

    modifying html and adding classes

--- src/project.js
+++ src/project.js
@@ -1,3 +1,14 @@
-let apiKey = "bf2bdff99ecfdbc9e74d435ee65cf081";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Rotterdam&appid=${apiKey}`;
+function displayTemp(response) {
+    console.log(response.data.main.temp);
+
+}
+
+
+let apiKey = `bf2bdff99ecfdbc9e74d435ee65cf081`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Rotterdam&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemp);
+
+
+
 
