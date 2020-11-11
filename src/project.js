f56fa741a80b7220ed622614e4805953fdbf72fe
D Janssen <doreenmarie_cee@hotmail.com>
2020-11-11 22:32:55 +0100

    adding real-time date and time

--- src/project.js
+++ src/project.js
@@ -1,5 +1,21 @@
+function formatDate(timestamp) {
+    let date = new Date (timestamp);
+    let hours = date.getHours();
+    if (hours < 10) hours = `0${hours}`
+
+    let minutes = date.getMinutes();
+    if (minutes < 10) hours = `0${minutes}`
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+
+    return `${day} ${hours}: ${minutes}`;
+
+}
+
+
+
 function displayTemp(response) {
-    console.log(response.data);
     let temperature = document.querySelector("#temperature");
     temperature.innerHTML = Math.round (response.data.main.temp);
 
@@ -17,6 +33,9 @@ function displayTemp(response) {
 
     let wind = document.querySelector("#wind");
     wind.innerHTML = Math.round (response.data.wind.speed);
+
+    let today = document.querySelector("#today");
+    today.innerHTML = formatDate(response.data.dt*1000);
 
 
 }
