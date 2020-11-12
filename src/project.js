70ca922e75f22d3e40bea715baebded28ba164c2
D Janssen <doreenmarie_cee@hotmail.com>
2020-11-12 21:06:13 +0100

    changing the codes for the icon to work

--- src/project.js
+++ src/project.js
@@ -38,7 +38,8 @@ function displayTemp(response) {
     today.innerHTML = formatDate(response.data.dt*1000);
 
     let icon = document.querySelector("#icon");
-    icon.setAttribute("src",`http://openweathermap.org/img/wn/04n@2x.png`); 
+    icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+    icon.setAttribute("alt", response.data.weather[0].description);
 
 
 }
